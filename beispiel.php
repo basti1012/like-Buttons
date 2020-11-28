a9815a6ea049948583a69ef3e9a1872e889c3c09
basti1012 <48145852+basti1012@users.noreply.github.com>
2020-11-28 02:43:55 +0100

    Create beispiel.php

---
+++ beispiel.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Like and Dislike system</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="stylesheet" href="like.css">
+</head>
+<body>
+<div class="posts-wrapper">
+<?php 
+$id=2;
+require('include.php'); 
+?>
+
+</div>
+<script src="like.js"></script>
+</body>
+</html>
